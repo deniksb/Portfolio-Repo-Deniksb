66162641fed7bd696744e5b421a72a7c06898739
deniksb <58732525+deniksb@users.noreply.github.com>
2021-03-22 13:24:54 +0200

    Update script.js

--- html/script.js
+++ html/script.js
@@ -428,7 +428,8 @@ function stopFlying(duration) {
 
 //function for calling the flying block
 var intervalIdBlock = window.setInterval(function () {
-    if(coin.style.display == "none"){
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(coin.style.display == "none" &&  blockLeft < 430){
          var randomSpeed = Math.random() * (1500 - 500) + 500;
     flyingBlock.style.animationDuration = randomSpeed + "ms";
     flyingBlock.style.display = "block";
