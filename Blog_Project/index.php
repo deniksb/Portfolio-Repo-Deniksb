51447178034c19adba02f0840dac329b8c64e3d0
deniksb <58732525+deniksb@users.noreply.github.com>
2022-02-26 19:48:07 +0200

    Working database and fetching posts from db

--- Blog_Project/index.php
+++ Blog_Project/index.php
@@ -14,7 +14,7 @@
 </head>
 <body>
      <!-- navbar -->
-    <nav id="navbar" class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav id="navbar" class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
         <a id="logo" class="navbar-brand text-primary" href="#">Hackr</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -22,10 +22,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="index.html#home">Home<span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="index.php#home">Home<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link" href="#">Posts<span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php#post-box">Posts<span class="sr-only">(current)</span></a>
               </li>
             <li class="nav-item">
               <a class="nav-link" href="https://www.kali.org/" target="_blank">Kali Linux</a>
@@ -38,17 +38,63 @@
         </div>
       </nav>
       <div id="home">
+        <img src="logo.png" alt="logo" class="img-fluid">
         <br>
         <br>
     <h1 class="text-primary">Welcome to the world's best blog for hackers!</h1>
     <p>Here you can find up to date information on all things related to penetration testing and ethical hacking. <br> Enjoy!</p>    
     </div>
-      
+  <!-- carousel -->
+    <div id="carousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="kali.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="code.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="anon.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<br>
+<br>
+
+    <div id="post-box">
+      <?php include 'getdata.php';?>
+    </div>
+    
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2022
+    <a href="https://www.linkedin.com/in/denislav-berberov/" target="_blank"> Denislav Berberov</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+<!-- Footer -->
       
 
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
 </body>
 </html>
